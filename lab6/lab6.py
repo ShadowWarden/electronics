958a92b2316da275715a40ed9ceda6b5c9f13059
Omkar Ramachandran <omkar.ramachandran@gmail.com>
2018-02-26 21:19:14 -0700

    Added data for Lab 6

---
+++ lab6/lab6.py
@@ -0,0 +1,17 @@
+# Step 1
+Vpk_pk2 = 216e-3
+Vpk_pk1 = 212e-3
+
+# Component Values
+R = 1.481e6
+Rf = 1.5081e6
+Cf = 10.27e-12
+
+# 3db Measurement
+fb = 11400
+
+# Step 2
+# Light-on measurement
+Vout = 0.968
+# Light-off Mesaurement
+Vout = 0.08
